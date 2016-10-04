2683bea9e551af3e7c2270ab86c0eede5b296406
Aneej Matthai <AneejM@gmail.com>
2016-10-04 09:17:03 +0530

    ketword class was missing - added it

--- Person.java
+++ Person.java
@@ -1,12 +1,14 @@
-public Person {
+public class Person {
 	
 	int age ;
 
-    // getter and setter methods for variable age
+        // getter and setter methods for variable age
 	public int getAge() {
 		return age ;
 	}
 	public void setAge(int a) {
 		age = a ;
 	}
+
+	String name ;
 }
